29495e024f7ab1c68d45b2f1aaf65419100731fb
joelDeville <devilleprogramming@gmail.com>
2022-09-11 17:25:56 -0500

    Updates NoteTaker.java
    Cleaned up file and ensured execution.

--- NoteTaker.java
+++ NoteTaker.java
@@ -1,53 +1,49 @@
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.GridPane;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.text.Text;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class NoteTaker extends Application
-{
-	@Override
-	public void start(Stage stage)
-	{
-		// note display pane and entire window pane created
-		VBox display = new VBox(5);
-		VBox outline = new VBox();
-		
-		// ScrollPane created to accomodate more notes in the display
-		// binds height of scroll to match window's current height
-		// but also restricts so text entry is available at the bottom
-		ScrollPane scroll = new ScrollPane(display);
-		scroll.prefHeightProperty().bind(stage.heightProperty());
-		scroll.setMaxHeight(600);
-		
-		// adding padding of elements within the screen
-		display.setPadding(new Insets(10));
-		outline.setPadding(new Insets(10));
-		
-		TextField input = new TextField();
-		input.setPrefWidth(300);
-		
-		// controller for user hitting enter or return
-		// adds user's input into display VBox and clears for next entry
-		input.setOnAction((ActionEvent e) ->
-		{
-			display.getChildren().add(new Text(input.getText()));
-			input.clear();
-		});
-		
-		// adds scrollpane of notes and input field into general screen
-		outline.getChildren().addAll(scroll, input);
-		
-		// sets initial size of screen and displays the application
-		Scene scene = new Scene(outline, 600, 300);
-		stage.setScene(scene);
-		stage.setTitle("Notes");
-		
-		stage.show();
-	}
+public class NoteTaker extends Application {
+    @Override
+    public void start(Stage stage) {
+        // note display pane and entire window pane created
+        VBox display = new VBox(5);
+        VBox outline = new VBox();
+
+        // ScrollPane created to accomodate more notes in the display
+        // binds height of scroll to match window's current height
+        // but also restricts so text entry is available at the bottom
+        ScrollPane scroll = new ScrollPane(display);
+        scroll.prefHeightProperty().bind(stage.heightProperty());
+        scroll.setMaxHeight(600);
+
+        // adding padding of elements within the screen
+        display.setPadding(new Insets(10));
+        outline.setPadding(new Insets(10));
+
+        TextField input = new TextField();
+        input.setPrefWidth(300);
+
+        // controller for user hitting enter or return
+        // adds user's input into display VBox and clears for next entry
+        input.setOnAction((ActionEvent e) -> {
+            display.getChildren().add(new Text(input.getText()));
+            input.clear();
+        });
+
+        // adds scrollpane of notes and input field into general screen
+        outline.getChildren().addAll(scroll, input);
+
+        // sets initial size of screen and displays the application
+        Scene scene = new Scene(outline, 600, 300);
+        stage.setScene(scene);
+        stage.setTitle("Notes");
+
+        stage.show();
+    }
 }
